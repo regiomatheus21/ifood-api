950a53dccdcc77ec8461f9ede40af85d1db318a3
Regio Matheus <regio.pimentel2016@gmail.com>
2024-03-18 20:57:59 -0300

    inicia projeto

---
+++ src/test/java/com/ifood/ifoodapi/IfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifood.ifoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
