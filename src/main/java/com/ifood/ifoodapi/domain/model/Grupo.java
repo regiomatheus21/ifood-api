b483cf3223f0074f87d6d2379b13023858dcffa4
regio.matheus <regio.delima@socin.com.br>
2026-01-21 21:32:34 -0300

    feat: criacao de relacionamento entre objetos

---
+++ src/main/java/com/ifood/ifoodapi/domain/model/Grupo.java
@@ -0,0 +1,22 @@
+package com.ifood.ifoodapi.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Grupo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+
+}
