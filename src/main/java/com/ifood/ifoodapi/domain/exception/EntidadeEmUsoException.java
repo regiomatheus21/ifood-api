a9ab459ab7bed7337b7942bd2a5b609028424e6b
regio.matheus <regio.delima@socin.com.br>
2026-02-19 21:14:37 -0300

    feat: implementa tratamento de excessao

--- src/main/java/com/ifood/ifoodapi/domain/exception/EntidadeEmUsoException.java
+++ src/main/java/com/ifood/ifoodapi/domain/exception/EntidadeEmUsoException.java
@@ -1,5 +1,9 @@
 package com.ifood.ifoodapi.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class EntidadeEmUsoException extends RuntimeException{
     private static long serialVersionUID= 1L;
 
